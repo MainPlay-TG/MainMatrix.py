19767bb34f900db89eeba65356563b6c4a174040
MainPlay-TG <xbox.roman6666666666@gmail.com>
2024-05-09 14:00:55 +0700

    Run autopep8

--- src/MainMatrix/__init__.py
+++ src/MainMatrix/__init__.py
@@ -2,35 +2,35 @@
 \u0420\u0430\u0437\u0440\u0430\u0431\u043E\u0442\u0447\u0438\u043A: MainPlay TG
 https://t.me/MainPlay_InfoCh"""
 
-__version_tuple__=(0,0,1)
-__import_data__={
-  "{name}=main.{name}":[
-    "DictWithStr",
-    "DictWithTuple",
-    "DontFill",
-    "ListsInList",
-    "matrix",
-    "PillowImage",
-    "WrongCoordinates",
+__version_tuple__ = (0, 0, 1)
+__import_data__ = {
+    "{name}=main.{name}": [
+        "DictWithStr",
+        "DictWithTuple",
+        "DontFill",
+        "ListsInList",
+        "matrix",
+        "PillowImage",
+        "WrongCoordinates",
     ]
-  }
-__depends__={
-  "required":[],
-  "optional":[
-    "PIL",
+}
+__depends__ = {
+    "required": [],
+    "optional": [
+        "PIL",
     ]
-  }
-__scripts__=[]
-__all__=[]
-__import_errors__={}
+}
+__scripts__ = []
+__all__ = []
+__import_errors__ = {}
 import MainMatrix.main as main
-for code,names in __import_data__.items():
+for code, names in __import_data__.items():
   for name in names:
     try:
       exec(code.format(name=name))
       __all__.append(name)
     except Exception as e:
-      __import_errors__[name]=e
+      __import_errors__[name] = e
 __all__.sort()
-__version__="{}.{}.{}".format(*__version_tuple__)
-del code,names,name
+__version__ = "{}.{}.{}".format(*__version_tuple__)
+del code, names, name
